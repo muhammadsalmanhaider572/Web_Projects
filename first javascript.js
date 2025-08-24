4c1e0877adbee8de3a72bdcef8f371b00c20f9a1
Muhammad Salman Haider <msalmanhaider572@gmail.com>
2025-08-24 21:42:02 +0500

    JS Practice

--- first javascript.js
+++ first javascript.js
@@ -1,5 +1,21 @@
-let arr = ['Google', 'Uber','IBM','Netflix','Bloomberg','Microsoft'];
-arr.shift();
-arr.splice(0,1,'Ola');
-arr.push('Amazon');
+
+let n = prompt ("Enter any numbers");
+
+let arr = [];
+
+for(let i=1; i<=n;i++){
+    arr[i-1] = i;
+}
 console.log(arr);
+
+let sum = arr.reduce((prev , curr)=>{
+       return prev+ curr;
+});
+
+console.log(sum);
+
+let pro = arr.reduce((prev , curr)=>{
+       return prev * curr;
+});
+
+console.log(pro);
