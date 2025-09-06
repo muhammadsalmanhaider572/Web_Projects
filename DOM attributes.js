19005b4b4bbe50f3a6536c7104dbe441eff5cecb
Muhammad Salman Haider <msalmanhaider572@gmail.com>
2025-09-06 09:59:57 +0500

    Event Practice

--- DOM attributes.js
+++ DOM attributes.js
@@ -10,15 +10,33 @@
 // id.style.textAlign = "center"
 
 
-let btn =document.createElement("button");
-btn.innerHTML = "Click me!";
-console.log(btn);
+// let btn =document.createElement("button");
+// btn.innerHTML = "Click me!";
+// console.log(btn);
 
-let id = document.querySelector("div");
-id.before(btn);
+// let id = document.querySelector("div");
+// id.before(btn);
 
-btn.style.color = "white";
-btn.style.backgroundColor = "red";
+// btn.style.color = "white";
+// btn.style.backgroundColor = "red";
 
-let parah = document.querySelector("p");
-console.log(parah.classList.add("newclass"));
+// let parah = document.querySelector("p");
+// console.log(parah.classList.add("newclass"));
+
+
+
+let modebtn = document.querySelector("#btn1");  // selects the button with id="btn1"
+let currMode = 'Light';                         // starting mode is Light
+
+modebtn.addEventListener("click", () => {
+    if (currMode == 'Light') {                  // if current mode is Light
+        currMode = "dark";                      // switch to dark
+        document.querySelector("body").style.backgroundColor = "black";
+    } else {                                    // if current mode is Dark
+        currMode = "Light";                     // switch back to light
+        document.querySelector("body").style.backgroundColor = "white";
+    }
+    console.log(currMode);                      // log mode in console
+});
+
+
